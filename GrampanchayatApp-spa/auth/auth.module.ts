e942dd35bb14494074dcac93fc34ec2dece043fc
ganeshkadam111 <40893318+ganeshkadam111@users.noreply.github.com>
2020-09-21 05:05:08 -0500

    added remaining files

---
+++ GrampanchayatApp-spa/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { MaterialModule } from '../module/material.module';
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
+  ]
+})
+export class AuthModule { }
