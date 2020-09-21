29bf1cc1fa1fc7a10743e1762e7af2baf361be0b
ganeshkadam111 <40893318+ganeshkadam111@users.noreply.github.com>
2020-09-21 05:34:49 -0500

    Add files via upload

---
+++ GrampanchayatApp-spa/src/app/auth/login/login.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  submitted = false;
+
+  countries: any = [
+    {
+      full: 'Great Britain',
+      short: 'GB'
+    },
+    {
+      full: 'United States',
+      short: 'US'
+    },
+    {
+      full: 'Canada',
+      short: 'CA'
+    }
+  ];
+
+  constructor(private formBuilder: FormBuilder, private router: Router) { }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      grampanchayatName: ['', Validators.required],
+    });
+  }
+
+  get data() { return this.loginForm.controls; }
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      return;
+    } else if (this.data.username.value === localStorage.getItem('username') &&
+     this.data.password.value === localStorage.getItem('password')) {
+      this.router.navigate(['/home']);
+    } else {
+      this.submitted = true;
+      this.router.navigate(['/home']);
+    }
+  }
+}
