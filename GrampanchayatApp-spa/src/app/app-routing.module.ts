4bc5b5f4a582f148a4050d7b36ab5a7c171ca14e
ganeshkadam111 <40893318+ganeshkadam111@users.noreply.github.com>
2020-09-26 21:28:48 +0530

    Add files via upload

--- GrampanchayatApp-spa/src/app/app-routing.module.ts
+++ GrampanchayatApp-spa/src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/login/login.component';
 import { ContentComponent } from './core/components/content/content.component';
 import { PageNotFoundComponent } from './core/pages/page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('./home/home.module')
+          .then(mod => mod.HomeModule)
+      }]
+  },
+  {
+    path: '',
+    component: AuthComponent,
+    children: [
+      {
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.module')
+          .then(mod => mod.AuthModule)
+      }]
+  },
   {
     path: '',
     component: ContentComponent,
