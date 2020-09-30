b0631efd26e770bf01d17cfbc1569919b36e42f4
ganeshkadam111 <40893318+ganeshkadam111@users.noreply.github.com>
2020-09-30 19:40:26 +0530

    Add files via upload

--- GrampanchayatApp-spa/src/app/core/core.module.ts
+++ GrampanchayatApp-spa/src/app/core/core.module.ts
@@ -1,21 +1,37 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ContentComponent } from './components/content/content.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { MaterialModule } from '../module/material.module';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { RouterModule } from '@angular/router';
-import { MaterialModule } from '../module/material.module';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-
+import { HeaderWithLoginComponent } from './components/header-with-login/header-with-login.component';
+import { HeaderWithoutLoginComponent } from './components/header-without-login/header-without-login.component';
 
 @NgModule({
-  declarations: [ContentComponent, HeaderComponent, FooterComponent, PageNotFoundComponent],
+  declarations: [
+    HeaderComponent, 
+    FooterComponent,
+    PageNotFoundComponent, 
+    HeaderWithLoginComponent, 
+    HeaderWithoutLoginComponent
+  ],
   imports: [
     CommonModule,
     MaterialModule,
-    RouterModule
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  exports: [ContentComponent, HeaderComponent, FooterComponent],
+  exports: [
+    HeaderComponent, 
+    FooterComponent,
+    PageNotFoundComponent, 
+    HeaderWithLoginComponent, 
+    HeaderWithoutLoginComponent
+  ],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class CoreModule { }
