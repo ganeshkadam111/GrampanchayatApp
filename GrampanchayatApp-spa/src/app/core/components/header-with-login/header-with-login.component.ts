f4d27bf6628f22cf4fa697b96e7096ece3148fee
ganeshkadam111 <40893318+ganeshkadam111@users.noreply.github.com>
2020-09-30 19:36:38 +0530

    Add files via upload

---
+++ GrampanchayatApp-spa/src/app/core/components/header-with-login/header-with-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-with-login',
+  templateUrl: './header-with-login.component.html',
+  styleUrls: ['./header-with-login.component.scss']
+})
+export class HeaderWithLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
