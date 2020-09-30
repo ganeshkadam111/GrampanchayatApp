ec514b5380d63d92ea86bab40502ee89a3a2758e
ganeshkadam111 <40893318+ganeshkadam111@users.noreply.github.com>
2020-09-30 19:42:26 +0530

    Add files via upload

--- GrampanchayatApp-spa/src/app/dashboard/dashboard-routing.module.ts
+++ GrampanchayatApp-spa/src/app/dashboard/dashboard-routing.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
+import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
   {
-      path: '',
-      component: DashboardComponent
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [
+      {
+        path: 'search',
+        component: SearchComponent
+      }
+    ]
   }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- GrampanchayatApp-spa/src/app/dashboard/dashboard.module.ts
+++ GrampanchayatApp-spa/src/app/dashboard/dashboard.module.ts
@@ -1,17 +1,25 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { MaterialModule } from '../module/material.module';
-
-
+import { CoreModule } from '../core/core.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SearchComponent } from './search/search.component';
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [
+    DashboardComponent,
+    SearchComponent
+  ],
   imports: [
     CommonModule,
     DashboardRoutingModule,
-    MaterialModule
-  ]
+    MaterialModule,
+    CoreModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class DashboardModule { }
