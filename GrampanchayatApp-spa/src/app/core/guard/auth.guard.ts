67dd574f0e6177fd3ecec40f2f57cc82567c2833
ganeshkadam111 <40893318+ganeshkadam111@users.noreply.github.com>
2020-10-04 11:57:51 +0530

    Add files via upload

--- GrampanchayatApp-spa/src/app/core/guard/auth.guard.ts
+++ GrampanchayatApp-spa/src/app/core/guard/auth.guard.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../service/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(
+    public authService: AuthService,
+    public router: Router
+  ) { }
+
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.authService.isLoggedIn !== true) {
+      window.alert("Access not allowed!");
+      this.router.navigate(['auth/login'])
+    }
     return true;
   }
   
