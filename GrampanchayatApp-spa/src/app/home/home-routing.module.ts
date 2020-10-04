c65c49910fc6c1c2ee57f31098e1ae47dd1dbc1a
ganeshkadam111 <40893318+ganeshkadam111@users.noreply.github.com>
2020-10-04 12:08:18 +0530

    Add files via upload

--- GrampanchayatApp-spa/src/app/home/home-routing.module.ts
+++ GrampanchayatApp-spa/src/app/home/home-routing.module.ts
@@ -1,13 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ApplyComponent } from './apply/apply.component';
 import { HomeComponent } from './home.component';
+import { SearchComponent } from './search/search.component';
 
 
 const routes: Routes = [
+  // {
+  //   path: 'home/search',
+  //   component: HomeComponent
+  // },
   {
     path: 'home',
-    component: HomeComponent
-  },
+    component: HomeComponent,
+    children: [
+      {
+        path: 'search',
+        component: SearchComponent
+      },
+      {
+        path: 'apply',
+        component: ApplyComponent
+      }
+    ]
+  }
 ];
 
 @NgModule({
--- GrampanchayatApp-spa/src/app/home/home.module.ts
+++ GrampanchayatApp-spa/src/app/home/home.module.ts
@@ -5,10 +5,12 @@ import { HomeRoutingModule } from './home-routing.module';
 import { MaterialModule } from '../module/material.module';
 import { HomeComponent } from './home.component';
 import { CoreModule } from '../core/core.module';
+import { SearchComponent } from './search/search.component';
+import { ApplyComponent } from './apply/apply.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, SearchComponent, ApplyComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
