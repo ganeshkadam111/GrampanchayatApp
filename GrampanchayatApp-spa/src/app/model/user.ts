30090e042c8274a7f71f8990741a866614a0e31c
ganeshkadam111 <40893318+ganeshkadam111@users.noreply.github.com>
2020-10-04 12:10:27 +0530

    Add files via upload

---
+++ GrampanchayatApp-spa/src/app/model/user.ts
@@ -0,0 +1,16 @@
+export class User {
+    id: String;
+    firstName: String;
+    middelNmae: String
+    lastName: String;
+    contactNo: Number;
+    aadharNo: Number;
+    startDate: String;
+    endDate: String;
+    email: String;
+    districtId: String;
+    talukaId: String;
+    villageId: String;
+    userName: String;
+    password: String;
+}
